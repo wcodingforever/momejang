0ed2f178bced436923f0adc80e83db85efe9ffb7
Alex <alexoh@gmail.com>
2017-04-11 21:31:06 +0900

    Added page to include users. Still have to connect it to server.

---
+++ www/js/utils.js
@@ -0,0 +1,36 @@
+// The sendWS function should be called with an object parameter. The object format
+// should be have a string event parameter, along with a payload object that contains
+// the data required by the server. For example, to create a new user:
+//
+// var messageObj = {
+//     event: "createnewuser"
+//     , payload: {
+//         username: "peterpan"
+//         , firstname: "Peter"
+//         , lastname: "Earl"
+//         , password: "$#&@*`\t!%^()_-=+|?\r;][\""
+//     }
+// };
+
+// sendWs(messageObj, callback);
+//
+function sendWS(inObj, callback) {
+    if ("WebSocket" in window) {
+        ws = new WebSocket("ws://192.168.1.193:8001/");
+        // ws = new WebSocket("ws://192.168.1.119:8001/");
+
+        ws.onopen = function() {
+            var messageStr = JSON.stringify(messageObj);
+            ws.send(messageStr);
+            console.log("Message is sent...[" + messageStr + "]");
+        };
+            
+        ws.onmessage = function (evt) { 
+            console.log("Message is received..." + evt.data);
+            callback(evt.data)
+        };
+            
+        ws.onclose = function() { console.log("Connection is closed..."); };
+    }
+    else { alert("WebSocket NOT supported by your Browser!"); }
+}
